82279045522aba7a29bb7285f3542f6a44eeca20
Connor Home <connorhome1990@gmail.com>
2025-10-07 12:07:43 +0100

    Setup react testing library

--- vite.config.ts
+++ vite.config.ts
@@ -1,4 +1,4 @@
-import { defineConfig } from "vite";
+import { defineConfig, type UserConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import { tanstackRouter } from "@tanstack/router-plugin/vite";
 import tailwindcss from "@tailwindcss/vite";
@@ -14,4 +14,9 @@ export default defineConfig({
     }),
     react(),
   ],
-});
+  test: {
+    globals: true,
+    environment: "jsdom",
+    setupFiles: "./src/tests/setup.ts",
+  },
+} as UserConfig);
